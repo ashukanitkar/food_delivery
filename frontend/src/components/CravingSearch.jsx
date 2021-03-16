f15f18a6d2c307932cd5c29287844adda5e5e843
ashukanitkar <ak2729@cornell.edu>
2021-03-15 21:35:41 -0400

    updated styling for food search page

--- frontend/src/components/CravingSearch.jsx
+++ frontend/src/components/CravingSearch.jsx
@@ -2,13 +2,16 @@
 const CravingSearch = () => {
     return (
         <section class="section">
-        <div class="columns">
+        <div class="columns is-vcentered">
             <div class="column is-6">
                 <h2 class="title">What are you craving today?</h2>
                 <p>Food and more available for delivery and pickup</p>
             </div>
-            <div class="column is-6">
-                <input class="input is-warning" type="text" placeholder="Search"></input>
+            <div class="column is-5">
+                    <input class="input is-warning" type="text" placeholder="Search"></input>
+            </div>
+            <div class="column">
+                <button class="button is-medium is-lemon">Search</button>
             </div>
         </div>
         </section>
--- frontend/src/components/TopCategories.jsx
+++ frontend/src/components/TopCategories.jsx
@@ -1,27 +1,30 @@
-import burger from "../resources/y-burger.png"
-import fish from "../resources/y-fish.png"
-import salad from '../resources/y-salad.png'
-import spagetti from '../resources/y-pasta.png'
-import sandwich from '../resources/y-sandwich.png'
-import yPizza from '../resources/y-pizza.png'
+import sandwich from '../resources/sandwich.png';
+import burger from '../resources/burger.png';
+import fish from '../resources/fish.png';
+import spagetti from '../resources/spaguetti.png';
+import pizza from '../resources/pizza.png';
+import salad from '../resources/salad.png';
 
 const TopCategories = ({title, imageURL, date}) => {
     return (
-        <div className="card__wrapper column is-2">
-        <div className="card has-text-centered">
+        <div className="card__wrapper column is-1">
+        <div className="card lemon-outline">
             <div className="card-image">
-                <div className="columns">
-                    <div className="column is-3"></div>
-                <div className="column is-6">
-                <figure className="image is-3by2">
-                    <img src={imageURL} alt={title}/>
-                </figure>
+                <div className="columns is-centered">
+                <div className="column is-8">
+                    <figure className="image is-2by2">
+                        <img src={imageURL} alt={title}/>
+                    </figure>
                 </div>
                 </div>
             </div>
             <div className="card-content">
                 <div className="media-content">
-                    <p className="is-size-6 is-5">{title}</p>
+                    <div class="columns is-centered">
+                        <div class="column">
+                            <p className="is-size-6">{title}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -38,8 +41,13 @@ const Posts = () => (
             <TopCategories title="Burger" imageURL={burger} />
             <TopCategories title="Seafood" imageURL={fish} />
             <TopCategories title="Pasta" imageURL={spagetti} />
-            <TopCategories title="Pizza" imageURL={yPizza} />
+            <TopCategories title="Pizza" imageURL={pizza} />
             <TopCategories title="Salad" imageURL={salad} />
+            <TopCategories title="Seafood" imageURL={fish} />
+            <TopCategories title="Pasta" imageURL={spagetti} />
+            <TopCategories title="Seafood" imageURL={fish} />
+            <TopCategories title="Pasta" imageURL={spagetti} />
+            <TopCategories title="Pasta" imageURL={spagetti} />
         </div>
         </section>
         </div>
